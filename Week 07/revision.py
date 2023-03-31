ebe518c80c5f68c33805d745bab31b18e60471be
nbedrossian <nishantafe@gmail.com>
2023-03-31 15:43:50 +1100

    Initial commit

---
+++ Week 07/revision.py
@@ -0,0 +1,11 @@
+"""
+Login [l]
+Register [r]
+    Generate password [y]
+        Digits? [y/n]
+        Symbols? [y/n]
+        Length of password (default is 10) [50]
+    Custom password
+View Accounts [v]
+Exit [e]
+"""
